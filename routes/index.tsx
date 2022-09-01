89c950fab77e54e54e4538616fc745a3d581b273
Vinko Vacek <vinko.vacek@gmail.com>
2022-09-01 13:39:00 +0200

    image w&h, again?

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,8 +7,9 @@ export default function Home() {
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
         src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
+        width="40"
+        height="40"
+        alt="The fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
