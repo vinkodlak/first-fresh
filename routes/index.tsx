395cc4cf0861c1e9c31149d4b54574367481b352
Vinko Vacek <vinko.vacek@gmail.com>
2022-09-01 13:25:30 +0200

    remove counter

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,22 +1,19 @@
 /** @jsx h */
 import { h } from "preact";
 import { tw } from "@twind";
-import Counter from "../islands/Counter.tsx";
 
 export default function Home() {
   return (
     <div class={tw`p-4 mx-auto max-w-screen-md`}>
       <img
         src="/logo.svg"
-        width="40"
-        height="40"
+        height="100px"
         alt="the fresh logo: a sliced lemon dripping with juice"
       />
       <p class={tw`my-6`}>
         Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
         file, and refresh.
       </p>
-      <Counter start={3} />
     </div>
   );
 }
