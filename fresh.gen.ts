3d0af2bc78e704c1b0e741de62069e30ea36fbc2
Vinko Vacek <vinko.vacek@gmail.com>
2022-09-02 10:58:54 +0200

    add category pages

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,14 +5,16 @@
 import * as $0 from "./routes/[name].tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/api/joke.ts";
-import * as $3 from "./routes/index.tsx";
+import * as $3 from "./routes/category/[cat].tsx";
+import * as $4 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
     "./routes/[name].tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/api/joke.ts": $2,
-    "./routes/index.tsx": $3,
+    "./routes/category/[cat].tsx": $3,
+    "./routes/index.tsx": $4,
   },
   islands: {},
   baseUrl: import.meta.url,
---
+++ routes/category/[cat].tsx
@@ -0,0 +1,7 @@
+/** @jsx h */
+import { h } from "preact";
+import { PageProps } from "$fresh/server.ts";
+
+export default function Category(props: PageProps) {
+  return <div>Category {props.params.name}</div>;
+}
