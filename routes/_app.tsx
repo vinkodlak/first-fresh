79c1d8d7aa52206428cca553c39a28292aebea71
Vinko Vacek <vinko.vacek@gmail.com>
2022-09-06 17:24:30 +0200

    add font

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -31,6 +31,16 @@ export default function App(props: AppProps) {
           crossOrigin="true"
         />
         <link rel="preconnect" href="https://images-ta.vinko.me" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Jura&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       <div
