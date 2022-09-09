f94ac3d8c0c7cb1071c7b941dd1dc93b5d585c88
Vinko Vacek <vinko.vacek@gmail.com>
2022-09-09 19:11:40 +0200

    db-test

--- routes/category/[cat].tsx
+++ routes/category/[cat].tsx
@@ -1,5 +1,44 @@
-import { PageProps } from "$fresh/server.ts";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Client } from "https://deno.land/x/postgres@v0.16.1/mod.ts";
+import { tw } from "../../utils/twind.ts";
 
-export default function Category(props: PageProps) {
-  return <div>Category {props.params.name}</div>;
+const PGDATABASE = "railway";
+const PGHOST = "containers-us-west-68.railway.app";
+const PGPASSWORD = "sHG7cxerKoA5TxA7XU2X";
+const PGPORT = 5620;
+const PGUSER = "postgres";
+
+interface Post {
+  id: number;
+  title: string;
+  slug: string;
+  date: string;
+}
+
+export const handler: Handlers<Post[] | null> = {
+  async GET(_, ctx) {
+    const client = new Client({
+      user: PGUSER,
+      password: PGPASSWORD,
+      database: PGDATABASE,
+      hostname: PGHOST,
+      port: PGPORT,
+    });
+    await client.connect();
+    const object_result = await client.queryObject(
+      "SELECT * FROM posts2",
+    );
+    console.log(object_result.rows); // [{id: 1, name: 'Carlos'}, {id: 2, name: 'John'}, ...]
+
+    await client.end();
+    return ctx.render(object_result.rows as Post[]);
+  },
+};
+
+export default function Category({ data }: PageProps<number | null>) {
+  if (!data) {
+    return <h1>User not found</h1>;
+  }
+
+  return <div class={tw`col-span-full`}>Category: {JSON.stringify(data)}</div>;
 }
